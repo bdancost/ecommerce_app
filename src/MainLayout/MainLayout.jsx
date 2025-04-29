5c4fa5d01070aebd8226a7b6d9e3ad5bff178d4f
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-04-29 06:30:00 -0300

    Organizando as pastas

--- src/MainLayout/MainLayout.jsx
+++ src/MainLayout/MainLayout.jsx
@@ -1,5 +1,30 @@
+import { BrowserRouter, Routes, Route } from "react-router";
+import Navbar from "./../Components/Navbar/Navbar";
+import Home from "../Pages/Home/Home";
+import Auth from "../Pages/Auth/Auth";
+import Login from "../Pages/Auth/Login/Login";
+import Register from "../Pages/Auth/Register/Register";
+import Error from "../Pages/Error/Error";
+
 const MainLayout = () => {
-  return <div className="text-red-500">MainLayout</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        {/* <Navbar /> */}
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="auth" element={<Auth />}>
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
+          </Route>
+          {/* not found routes */}
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default MainLayout;
