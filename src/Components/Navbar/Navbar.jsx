9a3262d81e28c2577c0cf19dcd97f1d901518da4
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-04-30 19:13:07 -0300

    Ajustando o Navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,4 +1,11 @@
-import { Armchair, Check, Info } from "lucide-react";
+import {
+  Armchair,
+  Check,
+  Info,
+  Search,
+  ShoppingCart,
+  Heart,
+} from "lucide-react";
 import { Link } from "react-router";
 
 const Navbar = () => {
@@ -36,7 +43,7 @@ const Navbar = () => {
 
       {/* navbar middle */}
       <div className="navbar_middle flex items-center justify-center bg-[#f0f2f3] w-full h-[84px]">
-        <div className="lg:container">
+        <div className="lg:container grid grid-cols-3 justify-center items-center">
           <div className="logo_wrapper">
             <Link
               to="/"
@@ -48,13 +55,32 @@ const Navbar = () => {
           </div>
 
           <div className="search_box">
-            <form action="#" className="max-w-96 h-[44px]">
+            <form action="#" className="max-w-96 h-[44px] relative">
               <input
                 type="text"
                 placeholder="Search here..."
                 className="w-full h-full  bg-white rounded-lg pl-4"
               />
+
+              <button
+                className="absolute top-50% right-4 translate-y-1/2"
+                size="22px"
+                color="#272343"
+              >
+                <Search />
+              </button>
             </form>
+          </div>
+
+          {/* navbar middle right */}
+          <div className="navbar_middle_right">
+            <button className="btn capitalize">
+              <ShoppingCart /> cart{" "}
+              <div className="badge badge-sm bg-[#029fae]">2</div>
+            </button>
+            <button className="btn capitalize">
+              <Heart />
+            </button>
           </div>
         </div>
       </div>
