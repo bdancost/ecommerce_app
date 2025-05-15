eee8f73bf0608bed3e4d20f8286d69c6462bcfa9
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-05-15 20:49:49 -0300

    Ajustando component Client

--- src/Components/Client/Client.jsx
+++ src/Components/Client/Client.jsx
@@ -2,35 +2,35 @@ import SectionTitle from "../SectionTitle/SectionTitle";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
-import { ShoppingCart } from "lucide-react";
+import { ShoppingCart, User } from "lucide-react";
 
 const Client = () => {
   const clientSays = [
     {
       id: 1,
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.",
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.",
       name: "John Doe",
       position: "CEO, Company",
     },
     {
       id: 2,
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.",
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.",
       name: "John Doe",
       position: "CEO, Company",
     },
     {
       id: 3,
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.",
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.",
       name: "John Doe",
       position: "CEO, Company",
     },
     {
       id: 4,
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.",
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.",
       name: "John Doe",
       position: "CEO, Company",
     },
@@ -45,49 +45,24 @@ const Client = () => {
   };
 
   return (
-    <div>
+    <div className="lg:container mx-auto">
       <SectionTitle
         title="What is client says about us"
         mb="mb-11"
         textAlign="center"
       />
 
-      <div className="max-w-[1400px] slider-container features_slider w-full h-full">
+      <div className="max-w-[1400px] slider-container w-full h-full">
         <Slider {...settings}>
           {clientSays?.map((client, index) => (
             <div key={index} className="p-4">
-              <div className="feature_image mb-4 relative">
-                <img
-                  className="w-full h-full object-cover"
-                  src={client?.image}
-                  alt={client?.title}
-                />
-                {client?.status && (
-                  <div className="absolute top-4 left-4 bg-[#007580] text-white px-2 py-1 rounded-lg">
-                    <button className="text-sm font-inter font-normal">
-                      {client?.status}
-                    </button>
-                  </div>
-                )}
-              </div>
-
-              <div className="feature_content">
-                <div className="flex items-center justify-between">
-                  <h4 className="text-base text-[#007580] capitalize font-inter font-normal mb-4">
-                    {client?.title}
-                  </h4>
-                  <span className="bg-[#007580] h-[44px] w-[44px] rounded-lg flex items-center justify-center">
-                    <ShoppingCart size="1.5rem" color="#fff" />
-                  </span>
-                </div>
-                <p className="text-xl flex items-center gap-2 text-[#272343] font-semibold font-inter mb-4">
-                  {client?.price}
-                  {client?.currentPrice && (
-                    <span className="text-sm text-[#9a9caa] font-inter font-normal">
-                      {client?.currentPrice}
-                    </span>
-                  )}
-                </p>
+              <p className="text-2xl text-[#636270] font-inter font-normal client_say_description">
+                {client?.description}
+              </p>
+              <div className="flex items-center">
+                <h4>
+                  <User />
+                </h4>
               </div>
             </div>
           ))}
