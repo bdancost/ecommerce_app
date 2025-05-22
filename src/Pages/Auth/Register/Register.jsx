eac1797d1fae168337d5dce100a233cb45d226d5
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-05-22 06:25:58 -0300

    Adicionando page Register

--- src/Pages/Auth/Register/Register.jsx
+++ src/Pages/Auth/Register/Register.jsx
@@ -1,5 +1,19 @@
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <div className="lg:container mx-auto">
+      <h3>Register</h3>
+
+      <form action="#">
+        <input type="text" placeholder="Your Name..." />
+        <input type="email" placeholder="Your Name..." />
+        <button type="submit">Register</button>
+      </form>
+      <p>
+        Don't have an account?<Link to={"/auth/login"}>Login</Link>
+        {""}
+      </p>
+    </div>
+  );
 };
 
 export default Register;
