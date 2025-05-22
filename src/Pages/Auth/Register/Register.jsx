25be374441a16b4f8f11ad49c4745d71d4d14125
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-05-22 06:37:30 -0300

    Configurando page register

--- src/Pages/Auth/Register/Register.jsx
+++ src/Pages/Auth/Register/Register.jsx
@@ -1,17 +1,20 @@
+import { Link } from "react-router";
+
 const Register = () => {
   return (
-    <div className="lg:container mx-auto">
-      <h3>Register</h3>
-
-      <form action="#">
-        <input type="text" placeholder="Your Name..." />
-        <input type="email" placeholder="Your Name..." />
-        <button type="submit">Register</button>
-      </form>
-      <p>
-        Don't have an account?<Link to={"/auth/login"}>Login</Link>
-        {""}
-      </p>
+    <div className="lg:container mx-auto p-[80px]">
+      <div className="max-w-[648px] w-full min-h-[382px] p-[31px] mx-auto flex items-center justify-center flex-col rounded-lg border-[1px] border-[#9a9caa]">
+        <h3>Register</h3>
+        <form action="#" className="">
+          <input type="text" placeholder="Your Name..." />
+          <input type="email" placeholder="Your Name..." />
+          <button type="submit">Register</button>
+        </form>
+        <p>
+          Don't have an account?<Link to={"/auth/login"}>Login</Link>
+          {""}
+        </p>
+      </div>
     </div>
   );
 };
