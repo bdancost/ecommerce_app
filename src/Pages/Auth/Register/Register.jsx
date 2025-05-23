26c11b4db4dce76e65864c313cf860c529fdede5
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-05-23 17:44:35 -0300

    Configurando page Register

--- src/Pages/Auth/Register/Register.jsx
+++ src/Pages/Auth/Register/Register.jsx
@@ -4,11 +4,29 @@ const Register = () => {
   return (
     <div className="lg:container mx-auto p-[80px]">
       <div className="max-w-[648px] w-full min-h-[382px] p-[31px] mx-auto flex items-center justify-center flex-col rounded-lg border-[1px] border-[#9a9caa]">
-        <h3>Register</h3>
-        <form action="#" className="">
-          <input type="text" placeholder="Your Name..." />
-          <input type="email" placeholder="Your Name..." />
-          <button type="submit">Register</button>
+        <h3 className="text-3xl text-[#272343] font-semibold font-inter capitalize mb-5">
+          Register
+        </h3>
+        <form
+          action="#"
+          className="flex flex-col items-center w-full space-y-4"
+        >
+          <input
+            type="text"
+            placeholder="Your Name..."
+            className="w-full h-[50px] bg-[#f0f2f3] rounded-lg pl-3.5"
+          />
+          <input
+            type="email"
+            placeholder="Your Name..."
+            className="w-full h-[50px] bg-[#f0f2f3] rounded-lg pl-3.5"
+          />
+          <button
+            type="submit"
+            className="w-full h-[50px] bg-[#007580] rounded-lg pl-3.5"
+          >
+            Register
+          </button>
         </form>
         <p>
           Don't have an account?<Link to={"/auth/login"}>Login</Link>
